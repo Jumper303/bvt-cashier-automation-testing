c5b7042005db66c3d0c99303ee3e7362ca3735d9
peter.dobrosi <peter.dobrosi@gmail.com>
2015-08-08 23:26:01 +0200

    Update to local execution

--- start-selenium-grid-and-node.bat
+++
@@ -1,3 +0,0 @@
-start java -jar selenium\selenium-server-standalone-2.47.1.jar -role hub -host 127.0.0.1
-start java -jar selenium\selenium-server-standalone-2.47.1.jar -role node  -hub http://127.0.0.1:4444/grid/register 
-rem -Dwebdriver.chrome.driver=selenium\chromedriver.exe
