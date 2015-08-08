e24f93e544f3335a722b693fa6fd366bd45e01f9
peter.dobrosi <peter.dobrosi@gmail.com>
2015-08-08 23:32:39 +0200

    Cleanup

--- src/test/java/com/bvt/cashier/test/acceptance/common/TestFixture.java
+++ src/test/java/com/bvt/cashier/test/acceptance/common/TestFixture.java
@@ -1,65 +1,32 @@
 package com.bvt.cashier.test.acceptance.common;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-
-import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 
 public abstract class TestFixture {
 	protected WebDriver driver = null;
-	private DesiredCapabilities capabilities = null;
-
-	@Parameters({ "hubUrl", "driverLocation", "browser", "os", "version" })
+	
+	@Parameters({ "driverLocation", "browser" })
 	@BeforeClass
-	public void setUp(String hubUrl, String driverLocation, String browser, String os, String version) {
-		Platform platform = null;
-
+	public void setUp(String driverLocation, String browser) {
 		if (browser.equalsIgnoreCase("firefox")) {
-			capabilities = DesiredCapabilities.firefox();
+			driver = new FirefoxDriver();
 		} else if (browser.equalsIgnoreCase("internet explorer")) {
-			capabilities = DesiredCapabilities.internetExplorer();
+			driver = new InternetExplorerDriver();
 			if (driverLocation != null && !driverLocation.isEmpty()) {
 				System.setProperty("webdriver.ie.driver", driverLocation);
 			}
-		} else if (browser.equalsIgnoreCase("chrome")) {
-			capabilities = DesiredCapabilities.chrome();
+		} else if (browser.equalsIgnoreCase("chrome")) {			
 			if (driverLocation != null && !driverLocation.isEmpty()) {
 				System.setProperty("webdriver.chrome.driver", driverLocation);
+				driver = new ChromeDriver();
 			}
 		}
-
-		if (version != null && !version.isEmpty()) {
-			capabilities.setVersion(version);
-		}
-
-		if (os.equalsIgnoreCase("Windows7")) {
-			platform = Platform.WINDOWS;
-		} else if (os.equalsIgnoreCase("Vista")) {
-			platform = Platform.VISTA;
-		} else if (os.equalsIgnoreCase("Linux")) {
-			platform = Platform.LINUX;
-		}
-
-		if (platform != null) {
-			capabilities.setPlatform(platform);
-		}
-
-		try {
-			driver = new RemoteWebDriver(new URL(hubUrl), capabilities);
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
 	}
 
 	@AfterClass
